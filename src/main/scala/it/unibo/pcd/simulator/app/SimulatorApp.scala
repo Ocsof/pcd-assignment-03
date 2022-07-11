7c91bf4fea5625c505f8e76e516a86548e41171b
Ocsof <francesco.foschini6@studio.unibo.it>
2022-07-11 18:52:39 +0200

    Doing test performance

--- src/main/scala/it/unibo/pcd/simulator/app/SimulatorApp.scala
+++ src/main/scala/it/unibo/pcd/simulator/app/SimulatorApp.scala
@@ -10,7 +10,8 @@ import scala.collection.mutable
 
   @main
   def main(): Unit = {
-    val system: ActorSystem[RootActorCommand] = ActorSystem(RootActor(Simulation(1000, 10000), true, 0, 0.0, mutable.Seq.empty, None, System.currentTimeMillis()), name = "simulation")
+    //mettere il terzo parametro true per attivare la view
+    val system: ActorSystem[RootActorCommand] = ActorSystem(RootActor(Simulation(5000, 10000), false, 0, 0.0, mutable.Seq.empty, None, System.currentTimeMillis()), name = "simulation")
     system ! StartSimulation
   }
 
